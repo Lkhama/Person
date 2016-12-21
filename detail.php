10ab1cebb84a544800b328b1a5b08ba5b14bcaaa
Lhamaa <blkhama@gmail.com>
2016-12-21 23:45:52 +0800

    HW1

--- detail.php
+++ detail.php
@@ -1,7 +1,7 @@
 <?php include 'header.php'; ?>
 <?php
 	include 'db_connection.php';
-	$sql = "SELECT * FROM person WHERE id=" . $_GET['id'];
+	$sql = 'SELECT * FROM person WHERE id=' . $_GET['id'];
 	$result = $conn->query($sql);
 ?>
 <?php if ($result->num_rows > 0) : ?>
@@ -20,13 +20,13 @@
 		</tr>
 		<tr>
 			<td align="right"><b>Age</b></td>
-			<td><?php echo (date("Y")-substr($row["bdate"], 0, 4)); ?></td>
+			<td><?php echo (date('Y')-substr($row['bdate'], 0, 4)); ?></td>
 		</tr>
 		<?php if (isset($_SESSION['name'])) : ?>
 		<tr>
 				<td colspan="2" align='center'>
-					<a href="edit.php?id=<?php echo $row["id"]; ?>">Edit</a>
-					<a href="delete.php?id=<?php echo $row["id"]; ?>" onclick='return confirm("Do you really wanna delete this person?");'>Delete</a>
+					<a href="edit.php?id=<?php echo $row['id']; ?>">Edit</a>
+					<a href="delete.php?id=<?php echo $row['id']; ?>" onclick='return confirm("Do you really wanna delete this person?");'>Delete</a>
 				</td>
 			</tr>
 		<?php endif; ?>
