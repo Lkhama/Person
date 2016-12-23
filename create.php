37124bec6ae2f34ab877dc9feadcedb14633c818
Lhamaa <blkhama@gmail.com>
2016-12-23 16:39:34 +0800

    Some syntax improvement and bug fix of html

--- create.php
+++ create.php
@@ -1,7 +1,7 @@
 <?php include 'header.php'; ?>
 <?php
 	include 'db_connection.php';
-	$sql = "INSERT INTO person (name, email, bdate) VALUES ('" . $_POST['name'] . "', '" . $_POST['email'] . "', '" . $_POST['byear'] . "-" . $_POST['bmonth'] . "-" . $_POST['bday'] . "')";
+	$sql = 'INSERT INTO person (name, email, bdate) VALUES ("' . $_POST['name'] . '", "' . $_POST['email'] . '", "' . $_POST['byear'] . '-' . $_POST['bmonth'] . '-' . $_POST['bday'] . '")';
 	if ($conn->query($sql) === TRUE) {
 		echo 'New person registered. <a href="index.php">Go to persons list</a>';
 	} else {
--- edit.php
+++ edit.php
@@ -13,28 +13,28 @@
 	<?php $row = $result->fetch_assoc(); ?>
 	<form action="update.php?id=<?php echo $row['id']; ?>" method='POST'>
 		<h1>Edit Person</h1>
-		Name: <input type="text" name='name' value='<?php echo $row['name']; ?>'></input>
+		Name: <input type="text" name="name" value="<?php echo $row['name']; ?>"></input>
 		</br>
-		E-mail: <input type="text" name='email' value='<?php echo $row['email']; ?>'></input>
+		E-mail: <input type="text" name="email" value="<?php echo $row['email']; ?>"></input>
 		</br>
 		Birthday:
 		Year:
 		<select name="byear">
 			<?php for ($i = 1950; $i <= 2016; $i=$i+1) : ?>
-				<option <?php if(substr($row["bdate"], 0, 4) == $i) { ?>selected="selected"<?php } ?><?php echo $i; ?></option>
+				<option <?php if(substr($row["bdate"], 0, 4) == $i) { ?>selected="selected"<?php } ?>><?php echo $i; ?></option>
 			<?php endfor; ?>
 		</select>
 		Month:
 		<select name="bmonth"></div></p>
 			<?php for ($i = 1; $i <= 12; $i=$i+1) : ?>
-				<option <?php if(substr($row["bdate"], 5, 2) == $i) { ?>selected="selected"<?php } ?><?php echo $i; ?></option>
+				<option <?php if(substr($row["bdate"], 5, 2) == $i) { ?>selected="selected"<?php } ?>><?php echo $i; ?></option>
 			<?php endfor; ?>
 		</select>
 		</select>
 		Day:
 		<select name="bday"></div></p>
 			<?php for ($i = 1; $i <= 31; $i=$i+1) : ?>
-				<option <?php if(substr($row["bdate"], 8, 2) == $i) { ?>selected="selected"<?php } ?><?php echo $i; ?></option>
+				<option <?php if(substr($row["bdate"], 8, 2) == $i) { ?>selected="selected"<?php } ?>><?php echo $i; ?></option>
 			<?php endfor; ?>
 		</select>
 		<input type="submit">
